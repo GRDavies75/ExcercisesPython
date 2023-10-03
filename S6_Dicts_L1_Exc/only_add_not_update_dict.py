db6d44fb9a4bbffec2500f1c482f25c440e19eb8
GRDavies75 <gdavies@home.nl>
2023-10-03 09:28:39 +0200

    Exc 6-35 only add not update dict

---
+++ S6_Dicts_L1_Exc/only_add_not_update_dict.py
@@ -0,0 +1,25 @@
+"""
+Excercise 6-35
+Given a key-value pair, only add to existing dict
+if given key is already part of dict, do nothing (don't update)
+"""
+
+
+def main() -> None:
+    # Chosen to use tuple for the key-value pair,
+    # could also have been a dictionary
+    input_data = [
+        ({"Januari": 45, "February": 56, "March": 67}, ("April", 67)),
+        ({"Januari": 45, "February": 56, "March": 67}, ("Januari", 67)),
+    ]
+
+    for init_dict, key_value_pair in input_data:
+        key, value = key_value_pair
+        if key not in init_dict:
+            init_dict[key] = value
+        
+        print(init_dict)
+
+
+if __name__ == "__main__":
+    main()
