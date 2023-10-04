06d33d1a3995a4473728ec626dd85718415c86e8
GRDavies75 <gdavies@home.nl>
2023-10-04 09:42:40 +0200

    Exc 6-38 & 6-39, find max or min value of a dict

---
+++ S6_Dicts_L1_Exc/find_the_dict_min_value.py
@@ -0,0 +1,27 @@
+"""
+Excercise 6-39
+Find the minimum value in a dict
+Assume all values are numerical
+Print None if dict is empty
+"""
+
+
+def main() -> None:
+    input_data = [
+        {"a": 4, "b": 3, "c":7},
+        {"a": 4, "b": 6},
+        {"a": 4, "b": 4},
+        {},
+    ]
+
+    for entry in input_data:
+        # Cornercase
+        if not entry:
+            print(None)
+            continue
+
+        print(min(entry.values()))
+
+
+if __name__ == "__main__":
+    main()
