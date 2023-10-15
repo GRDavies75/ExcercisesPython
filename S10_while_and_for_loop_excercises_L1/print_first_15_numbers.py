ebb29a922a45928e1f89a59d6d223a86a7fd160d
GRDavies75 <gdavies@home.nl>
2023-10-15 20:16:45 +0200

    Exc 10-58, print the first 15 numbers

---
+++ S10_while_and_for_loop_excercises_L1/print_first_15_numbers.py
@@ -0,0 +1,14 @@
+"""
+Excercise 10-58
+Print the first 15 numbers using a for loop and 
+making use of range()   
+"""
+
+
+def main() -> None:
+    for i in range(1, 16):
+        print(i)
+
+
+if __name__ == "__main__":
+    main()
