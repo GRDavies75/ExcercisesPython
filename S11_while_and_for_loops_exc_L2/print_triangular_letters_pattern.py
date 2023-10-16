b11c069887d219a7abe2cce3bb02de49e7be8455
GRDavies75 <gdavies@home.nl>
2023-10-16 15:30:07 +0200

    Exc 11-71 print a triangular letter pattern

---
+++ S11_while_and_for_loops_exc_L2/print_triangular_letters_pattern.py
@@ -0,0 +1,46 @@
+"""
+Excercise 11-71
+Print a triangular letters pattern for N rows
+
+eg, n = 5
+A
+B B
+C C C
+D D D D
+E E E E E
+"""
+import sys
+sys.path.append("..")
+from libs import input_prompter as ip
+
+
+def main() -> None:
+    UNICODE_A = 65
+    extra_information = [
+        "Print a triangular letters pattern for N rows",
+        "[Note: 1 <= N <= 26, Q = quit]",
+        "-------------------------------------"
+    ]
+    while True:
+        user_input = ip.get_user_input("For what 'N': ", extra_information)
+
+        # quiting clause
+        if user_input.casefold() == 'q':
+            break
+
+        if user_input.isdigit() and 1 <= int(user_input) <= 26:
+            # 26 letters in the western alphabet
+            total_rows = int(user_input)
+
+            for i in range (0, total_rows):
+                letter = chr(UNICODE_A + i) + " " 
+                pattern = letter * (i + 1)
+                print(pattern)
+        else:
+            print("Unexpected input")
+        # for readability
+        print()
+
+
+if __name__ == "__main__":
+    main()
