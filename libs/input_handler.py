b05ac8056263ca0b4c010262706c3dc2bd3e0849
GRDavies75 <gdavies@home.nl>
2023-10-16 10:20:16 +0200

    finalized input_handler

--- libs/input_handler.py
+++ libs/input_handler.py
@@ -4,13 +4,20 @@ A simplified unified user input requestor
 """
 
 
-def get_user_input(prompt='', *pre_prompts: str) -> str:
+def get_user_input(prompt: str='', *pre_prompts: str) -> str:
     """
     prompt is used at the input itself
-    pre_prompts gives the ability for extra sring before the input
+
+    pre_prompts gives the ability for extra strings before the input
+    
+    loose strings or a list of strings
     """
     for pre_prompt in pre_prompts:
-        print(pre_prompt)
+        if isinstance(pre_prompt, list):
+            for inner_elem in pre_prompt:
+                print(inner_elem)
+        else:
+            print(pre_prompt)
     user_input = input(prompt)
 
     return user_input
