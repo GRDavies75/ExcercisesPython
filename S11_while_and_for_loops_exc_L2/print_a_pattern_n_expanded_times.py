c5fd302bf42f4258a16abca534513456776ac2c9
GRDavies75 <gdavies@home.nl>
2023-10-16 13:46:19 +0200

    Excercise 11-66 print an expanding pattern

---
+++ S11_while_and_for_loops_exc_L2/print_a_pattern_n_expanded_times.py
@@ -0,0 +1,41 @@
+"""
+Excercise 11-66
+Print a pattern using loops (based on user-input)
+
+eg, give n = 3, expected output
+*
+**
+***
+
+"""
+import sys
+sys.path.append("..")
+from libs import input_prompter as ip
+
+
+def main() -> None:
+    PATTERN = '*'
+    extra_information = [
+        "Print an expanding pattern on n-lines",
+        "[Q = quit]",
+        "-------------------------------------"
+    ]
+    while True:
+        user_input = ip.get_user_input("For what 'n': ", extra_information)
+
+        # quiting clause
+        if user_input == 'q':
+            break
+
+        if user_input.isdigit() and int(user_input) >= 1:
+            int_user_input = int(user_input)
+            for i in range (1, int_user_input + 1):
+                print(PATTERN * i)
+        else:
+            print("Unexpected input")
+        # for readability
+        print()
+
+
+if __name__ == "__main__":
+    main()
