e330acc6e09cb8c0055c4d6e94849fe9802d95e4
GRDavies75 <gdavies@home.nl>
2023-10-16 14:38:20 +0200

    Exc 11-69 print a half pyramid

---
+++ S11_while_and_for_loops_exc_L2/print_a_half_pyramid_with_base_n.py
@@ -0,0 +1,52 @@
+"""
+Excercise 11-69
+Print a half-pyramid with a base (user-inputted) n
+
+eg, n = 5
+    *
+   **
+  ***
+ ****
+*****
+
+A bit of a misnomer, I don't consider this a 'half-pyramid'
+
+fun fact, use:
+WHITESPACE = ' ' (single space)
+PATTERN = '* ' (asterisk + a space)
+you actually get an real pyramid with base N
+"""
+import sys
+sys.path.append("..")
+from libs import input_prompter as ip
+
+
+def main() -> None:
+    PATTERN = "* " # a space for readability
+    WHITESPACE = "  " # Double space for readability
+    extra_information = [
+        "Print a reversed pyramid with a base n wide",
+        "[Q = quit]",
+        "-------------------------------------"
+    ]
+    while True:
+        user_input = ip.get_user_input("For what 'n': ", extra_information)
+
+        # quiting clause
+        if user_input == 'q':
+            break
+
+        if user_input.isdigit() and int(user_input) >= 1:
+            total_base_width = int(user_input)
+            for i in range (1, total_base_width + 1):
+                white_space_width = total_base_width - i
+                line_pattern = (WHITESPACE * white_space_width) + (PATTERN * i)
+                print(line_pattern)
+        else:
+            print("Unexpected input")
+        # for readability
+        print()
+
+
+if __name__ == "__main__":
+    main()
